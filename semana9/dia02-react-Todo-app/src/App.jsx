b4088592c78c2b9b9e55e0f8f1cec998cfcfb512
Jonathan Oblitas <oblitaskevin30@gmail.com>
2023-08-06 18:30:18 -0500

    "avanzando todo con react part1"

--- semana9/dia02-react-Todo-app/src/App.jsx
+++ semana9/dia02-react-Todo-app/src/App.jsx
@@ -1,34 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const DEFAULT_TODOS=[
+    {
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": true
+      },
+      {
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+      },
+      {
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+      },
+      {
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+      },
+      {
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+      },
+  ]
+  const [todos, setTodos] = useState(DEFAULT_TODOS);
+  const [input , setInput] = useState('');
+
+  // funciones
+    const handleChange = (event) =>{
+      const valueText = event.target.value;
+      // console.log(valueText)
+      setInput(valueText);
+    }
+
+    const handleSubmit = (event) =>{
+      event.preventDefault();
+      console.log('im working');
+      
+      const newTodo = {
+        "id": crypto.randomUUID(),
+        "title": input,
+        "completed": false
+        
+      }
+
+      setTodos([...todos, newTodo]);
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="w-full max-w-sm bg-red-500 mx-auto mt-10 rounded-lg p-4 shadow-lg border border-cyan-950">
+          <h1 className="text-2xl font-bold "> TODO APP </h1>
+          {/* {JSON.stringify(todos)} */}
+          
+          <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                className="w-full border my-3 p-3"
+                placeholder="que harás hoy ?"
+                onChange={handleChange}
+              />
+          </form>
+
+
+
+          <section className="mt-5">
+            <ul className="flex flex-col gap-3">
+                {todos.map(todo =>{
+                return (
+                  
+                  <li key={todo.id} 
+                  className={`text-stone-900 ${todo.completed ? 'line-through' : ''} `}  
+                  >
+                    <input type="checkbox" className="mr-4" />
+                    <span className="pr-3">{todo.title}</span>
+                    
+                  </li>)
+                })}
+
+            </ul>
+          </section>
+
+      </main>
+    
+    
     </>
+    
+
   )
 }
 
