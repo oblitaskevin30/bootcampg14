91b3fe30943271fea93dceeda2e3f52b8ddd199c
Jonathan Oblitas <oblitaskevin30@gmail.com>
2023-08-07 00:23:29 -0500

    finalizado el todoAPP semana9

--- semana9/dia02-react-Todo-app/src/App.jsx
+++ semana9/dia02-react-Todo-app/src/App.jsx
@@ -6,27 +6,27 @@ function App() {
 
   const DEFAULT_TODOS=[
     {
-      "id": 1,
+      "id": "1",
       "title": "delectus aut autem",
       "completed": true
       },
       {
-      "id": 2,
+      "id": "2",
       "title": "quis ut nam facilis et officia qui",
       "completed": false
       },
       {
-      "id": 3,
+      "id": "3",
       "title": "fugiat veniam minus",
       "completed": false
       },
       {
-      "id": 4,
+      "id": "4",
       "title": "et porro tempora",
       "completed": true
       },
       {
-      "id": 5,
+      "id": "5",
       "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
       "completed": false
       },
@@ -43,17 +43,62 @@ function App() {
 
     const handleSubmit = (event) =>{
       event.preventDefault();
-      console.log('im working');
+      console.log('Im working....');
       
       const newTodo = {
-        "id": crypto.randomUUID(),
-        "title": input,
-        "completed": false
+        "id": crypto.randomUUID(), // generamos el nuevo ID irrepetible
+        "title": input, // aca añadimos el titulo de la nueva tarea
+        "completed": false // se setea en false
         
       }
 
-      setTodos([...todos, newTodo]);
+      setTodos([...todos, newTodo])
+      setInput('')
     }
+
+
+    const handleCompleted =(event)=>{
+
+      const isChecked = event.target.checked
+      const idSelected= event.target.dataset.id
+
+      const newTodos = todos.map(todo => {
+        if (todo.id == idSelected){
+          return {...todo, completed: isChecked}
+        }
+
+        return todo
+      })
+
+      console.log(newTodos)
+      setTodos(newTodos)
+
+    }
+
+
+    const handleRemoveTodo = (event) => {
+      const idSelected = event.target.dataset.id
+  
+      const newTodos = todos.filter(todo => todo.id !== idSelected)
+  
+      console.log(newTodos)
+  
+      setTodos(newTodos)
+    }
+
+
+    const completedTodos = () =>{
+      const completed = todos.filter(todo => todo.completed === true)
+      return completed.length
+
+    }
+
+    const handleClearTodos = () =>{
+      const newTodos = todos.filter(todo => todo.completed === false)
+      setTodos(newTodos)
+    }
+
+
 
   return (
     <>
@@ -66,25 +111,52 @@ function App() {
                 type="text" 
                 className="w-full border my-3 p-3"
                 placeholder="que harás hoy ?"
+                required
                 onChange={handleChange}
+                value={input}
               />
           </form>
 
-
+          <div className="flex justify-between">
+          <span className="">{completedTodos()} / {todos.length}</span>
+          <button
+            className="bg-blue-500 rounded-lg px-2 py-1 text-white"
+            onClick={handleClearTodos}
+          >
+            Limpiar tareas completadas
+          </button>
+          </div>
 
           <section className="mt-5">
             <ul className="flex flex-col gap-3">
                 {todos.map(todo =>{
-                return (
-                  
-                  <li key={todo.id} 
-                  className={`text-stone-900 ${todo.completed ? 'line-through' : ''} `}  
-                  >
-                    <input type="checkbox" className="mr-4" />
-                    <span className="pr-3">{todo.title}</span>
+                  return (
                     
-                  </li>)
-                })}
+                    <li key={todo.id} 
+                    className= "flex" 
+                    >
+                      <input 
+                        type="checkbox" 
+                        className="mr-4"
+                        data-id = {todo.id}
+                        checked = {todo.completed}
+                        onChange={handleCompleted}
+                      />
+                      <div className="flex justify-between items-center w-full">
+                        <div className= {`text-stone-900 ${todo.completed ? 'line-through' : ''} `}  >
+                          {todo.title}
+                        </div>
+
+                        <button
+                        className="font-bold bg-red-300 rounded-lg px-2 py-2 text-white"
+                        data-id={todo.id}
+                        onClick={handleRemoveTodo}
+                        > ❎
+                        </button>
+                      </div>
+                    </li>
+                    )
+                  })}
 
             </ul>
           </section>
